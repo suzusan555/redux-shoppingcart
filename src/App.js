87ca57fff4133ef8936d365a8d94a95fbd3950f3
鈴木智昭 <suzukitomoaki@suzukitomoakinoMacBook-Air.local>
2023-01-31 09:45:27 +0900

    reducer dispatch action 作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, login } from "./actions"
 
 function App() {
+  const counter = useSelector((state) => state.counter)
+  const isLogin = useSelector((state) => state.isLogin)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
       <h1>Hello Redux</h1>
+      <h3>カウント: {counter}</h3>
+      <button onClick={() => dispatch(increment())}>＋</button>
+      <button onClick={() => dispatch(decrement())}>−</button>
+      {isLogin ? <h3>ログイン成功</h3> : <h3>ログインしてください</h3>}
+      <button onClick={() => dispatch(login())}>ログイン or ログアウト</button>
     </div>
   );
 }
